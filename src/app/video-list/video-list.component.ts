186301b097a25cd916f8692eeb6eb422a08b70e4
GeoAbin <georgeabin34@gmail.com>
2020-10-21 13:40:31 +0530

    C2

--- src/app/video-list/video-list.component.ts
+++ src/app/video-list/video-list.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {Video} from './../video'
 
 @Component({
   selector: 'app-video-list',
   templateUrl: './video-list.component.html',
   styleUrls: ['./video-list.component.css'],
-  inputs:['videos']
+  //inputs:['videos']
 })
 export class VideoListComponent implements OnInit {
+
+  @Input() videolist
 
   constructor() { }
 
   ngOnInit(): void {
+    console.warn(this.videolist)
   }
 
 }
